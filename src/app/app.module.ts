c6ea660032f8b9757eaecdba90a52a1472ffca5b
Jason Fu <gimpact709@gmail.com>
2023-06-26 01:41:37 +0200

    generated day componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { DayComponent } from './Components/day/day.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DayComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
