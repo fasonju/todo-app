88b59c096e110076afe7d03c1114fa807a822f34
Jason Fu <gimpact709@gmail.com>
2023-06-26 20:04:41 +0200

    supressed snake case error

--- src-tauri/src/Models/task.rs
+++ src-tauri/src/Models/task.rs
@@ -1,19 +1,20 @@
+/// Sqlite cannot store dates so we store as iso string
 use serde::{Deserialize, Serialize};
-
-/// SQLITE cannot store dates so we store as iso string
 use crate::diesel::{Insertable, Queryable};
 use crate::schema::tasks;
 
 #[derive(Queryable, Serialize, Deserialize)]
+#[allow(non_snake_case)]
 pub struct Task {
     id: i32,
     name: String,
-    due_date: String,
+    dueDate: String,
     completed: bool,
     text: String,
 }
 #[derive(Insertable, Serialize, Deserialize)]
 #[table_name = "tasks"]
+#[allow(non_snake_case)]
 pub struct InsertTask {
     name: String,
     dueDate: String,
