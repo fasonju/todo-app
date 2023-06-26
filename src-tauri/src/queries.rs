9d4fcb24acea4392dc6d69787eef47d40e9e59e5
Jason Fu <gimpact709@gmail.com>
2023-06-26 02:21:22 +0200

    prepare for sqlite

---
+++ src-tauri/src/queries.rs
@@ -0,0 +1,6 @@
+use rusqlite::{params, Connection, Result};
+
+/// * `iso_date` - The date to get tasks for in ISO format YYYY-MM-DDTHH:MM:SSZ
+pub async fn get_tasks_for_day(iso_date: String) -> Result<Vec<Task>, sqlx::Error> {
+    println("Getting tasks for day: {}", iso_date);
+}
--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { Task } from './Models/Task';
+import { invoke } from '@tauri-apps/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
+  /**
+   * 
+   * @param day day is in full JS fromat we convert this to iso format
+   */
   getTasksForDay(day: Date) : Observable<Task[]> {
-    throw new Error('Method not implemented.');
+    let iso_date : string = day.toISOString();
+    return from(invoke<Task[]>('get_tasks_for_day', {day: iso_date}));
   }
 
   constructor() { }
