bdd90fd335ed55f92f490d98438e0724a922c957
Jason Fu <gimpact709@gmail.com>
2023-06-27 21:30:36 +0200

    task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { DayComponent } from './Components/day/day.component';
+import { TaskComponent } from './Components/task/task.component';
 
 @NgModule({
-  declarations: [AppComponent, DayComponent],
+  declarations: [AppComponent, DayComponent, TaskComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
