69a4518e1b9a2f6c57e2d93542b4a1cbc5b20c10
Jason Fu <gimpact709@gmail.com>
2023-06-28 11:31:55 +0200

    date-picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 import { AppComponent } from "./app.component";
 import { DayComponent } from './Components/day/day.component';
 import { TaskComponent } from './Components/task/task.component';
+import { DatePickerComponent } from "./Components/date-picker/date-picker.component";
 
 @NgModule({
-  declarations: [AppComponent, DayComponent, TaskComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, DayComponent, TaskComponent, DatePickerComponent],
+  imports: [BrowserModule, FormsModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
